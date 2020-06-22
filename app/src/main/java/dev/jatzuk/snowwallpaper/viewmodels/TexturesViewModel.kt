17747147f43361eccf4eaeb32f5595ef31fdf48f
jatzuk <jatzuk.22@gmail.com>
2020-06-22 20:51:17 +0300

    TexturesViewModel.kt changed type to TextureCache

--- app/src/main/java/dev/jatzuk/snowwallpaper/viewmodels/TexturesViewModel.kt
+++ app/src/main/java/dev/jatzuk/snowwallpaper/viewmodels/TexturesViewModel.kt
@@ -1,6 +1,5 @@
 package dev.jatzuk.snowwallpaper.viewmodels
 
-import android.graphics.Bitmap
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dev.jatzuk.snowwallpaper.data.preferences.TextureCache
@@ -8,13 +7,11 @@ import dev.jatzuk.snowwallpaper.utilities.TextureProvider
 
 class TexturesViewModel : ViewModel() {
 
-    val textures = MutableLiveData<HashMap<TextureProvider.TextureType, Bitmap?>>()
-        get() {
-            field.value = TextureCache.getInstance().hashMap
-            return field
-        }
+    val textures = MutableLiveData<TextureCache>(TextureCache.getInstance())
 
     fun updateTexture(textureType: TextureProvider.TextureType) {
-        textures.value?.set(textureType, TextureCache.getInstance()[textureType])
+        val textureCache = TextureCache.getInstance()
+        textures.value?.set(textureType, textureCache[textureType])
+        textures.postValue(textureCache)
     }
 }
